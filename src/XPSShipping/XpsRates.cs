b6b72139b4051cc8c9c036effe84cd313e2f4f7f
Adam Humpherys <adam@adamh.us>
2019-11-07 09:41:13 -0700

    initial source commit

---
+++ src/XPSShipping/XpsRates.cs
@@ -0,0 +1,97 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace XPSShipping
+{
+    /// <summary>
+    /// Summary description for XpsRates
+    /// </summary>
+    public class XpsRates
+    {
+        private readonly string _apiKey;
+        private readonly string _customerId;
+
+        public XpsRates(string apiKey, string customerId)
+        {
+            _apiKey = apiKey;
+            _customerId = customerId;
+        }
+
+        public XpsRateQuotes Quotes(dynamic pkg) // change from dynamic to custom class
+        {
+            XpsRateQuotes quotes = new XpsRateQuotes();
+            var services = GetServices();
+            foreach (var s in services.Services)
+            {
+                if (s.CarrierCode == "dhl") // they don't want to use DHL at all
+                    continue;
+                quotes.Add(GetQuote(pkg));
+            }
+
+            return quotes;
+        }
+
+        private XpsRateQuote GetQuote(dynamic pkg) // change from dynamic to custom class (doesn't have to be the same type as Quotes())
+        {
+            byte[] body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(pkg));
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("https://xpsshipper.com/restapi/v1/customers/" + _customerId + "/quote");
+            req.Method = "POST";
+            req.ContentType = "application/json";
+            req.Headers.Add("Authorization", "RSIS " + _apiKey);
+            req.Accept = "application/json";
+            req.ContentLength = body.Length;
+
+            using (Stream s = req.GetRequestStream())
+                s.Write(body, 0, body.Length);
+
+            using (HttpWebResponse resp = (HttpWebResponse)req.GetResponse())
+            using (StreamReader sr = new StreamReader(resp.GetResponseStream()))
+                return JsonConvert.DeserializeObject<XpsRateQuote>(sr.ReadToEnd());
+        }
+
+        private XpsServices GetServices()
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("https://xpsshipper.com/restapi/v1/customers/" + _customerId + "/services");
+            req.Method = "GET";
+            req.ContentType = "application/json";
+            req.Headers.Add("Authorization", "RSIS " + _apiKey);
+            req.Accept = "application/json";
+
+            using (HttpWebResponse resp = (HttpWebResponse)req.GetResponse())
+            using (StreamReader sr = new StreamReader(resp.GetResponseStream()))
+                return XpsServices.FromJson(sr.ReadToEnd());
+        }
+    }
+
+    public class XpsRateQuotes : List<XpsRateQuote>
+    { }
+
+    public class XpsRateQuote
+    {
+        public string Currency { get; set; }
+        public string CustomsCurrency { get; set; }
+        public decimal TotalAmount { get; set; }
+        public decimal BaseAmount { get; set; }
+        public XpsSurcharge[] Surcharges { get; set; }
+        public string Zone { get; set; }
+        public XpsQuotePiece[] Pieces { get; set; }
+    }
+    public class XpsSurcharge
+    {
+        public string Description { get; set; }
+
+        public decimal Amount { get; set; }
+    }
+    public class XpsQuotePiece
+    {
+        public decimal TotalAmount { get; set; }
+        public decimal BaseAmount { get; set; }
+        public XpsSurcharge[] Surcharges { get; set; }
+    }
+}
---
+++ src/XPSShipping/XpsServices.cs
@@ -0,0 +1,117 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace XPSShipping
+{
+    public partial class XpsServices
+    {
+        [JsonProperty("services")]
+        public Service[] Services { get; set; }
+    }
+
+    public partial class Service
+    {
+        [JsonProperty("carrierCode")]
+        public string CarrierCode { get; set; }
+
+        [JsonProperty("carrierLabel")]
+        public string CarrierLabel { get; set; }
+
+        [JsonProperty("serviceLabel")]
+        public string ServiceLabel { get; set; }
+
+        [JsonProperty("serviceCode")]
+        public string ServiceCode { get; set; }
+
+        [JsonProperty("inbound")]
+        public bool Inbound { get; set; }
+
+        [JsonProperty("supportedCountries")]
+        public string[] SupportedCountries { get; set; }
+
+        [JsonProperty("unsupportedCountries")]
+        public string[] UnsupportedCountries { get; set; }
+
+        [JsonProperty("signatureOptions")]
+        public SignatureOption[] SignatureOptions { get; set; }
+
+        [JsonProperty("packageTypes")]
+        public PackageType[] PackageTypes { get; set; }
+    }
+
+    public partial class PackageType
+    {
+        [JsonProperty("packageTypeCode")]
+        public string PackageTypeCode { get; set; }
+
+        [JsonProperty("packageTypeLabel")]
+        public string PackageTypeLabel { get; set; }
+
+        [JsonProperty("multipieceSupported")]
+        public bool MultipieceSupported { get; set; }
+
+        [JsonProperty("weightUnit")]
+        public string WeightUnit { get; set; }
+
+        [JsonProperty("maxWeightPerPiece")]
+        public object MaxWeightPerPiece { get; set; }
+
+        [JsonProperty("dimType")]
+        public object DimType { get; set; }
+
+        [JsonProperty("dimUnit")]
+        public string DimUnit { get; set; }
+
+        [JsonProperty("presetLength")]
+        public object PresetLength { get; set; }
+
+        [JsonProperty("presetWidth")]
+        public object PresetWidth { get; set; }
+
+        [JsonProperty("presetHeight")]
+        public object PresetHeight { get; set; }
+    }
+
+    public partial class SignatureOption
+    {
+        [JsonProperty("signatureOptionCode")]
+        public string SignatureOptionCode { get; set; }
+
+        [JsonProperty("signatureOptionLabel")]
+        public string SignatureOptionLabel { get; set; }
+    }
+
+    public partial class XpsServices
+    {
+        public static XpsServices FromJson(string json)
+        {
+            return JsonConvert.DeserializeObject<XpsServices>(json, Converter.Settings);
+        }
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this XpsServices self)
+        {
+            return JsonConvert.SerializeObject(self, Converter.Settings);
+        }
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
